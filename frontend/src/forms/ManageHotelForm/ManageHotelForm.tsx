6b726d8d50b1bbf6fd0160d8e9863a608f922fa9
Prashistha <Prashisthaco@gmail.com>
2024-04-05 13:52:06 +0530

    Hotel view and edit feature completed

--- frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
+++ frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
@@ -4,6 +4,8 @@ import TypeSection from "./TypeSection";
 import FacilitiesSection from "./FacilitiesSection";
 import GuestsSection from "./GuestsSection";
 import ImagesSection from "./ImagesSection";
+import { HotelType } from "../../../../backend/src/shared/types";
+import { useEffect } from "react";
 
 export type HotelFormData = {
   name: string;
@@ -21,44 +23,50 @@ export type HotelFormData = {
 };
 
 type Props = {
-    // hotel?: HotelType;
-    onSave: (hotelFormData: FormData) => void;
-    isLoading: boolean;
-  };
+  hotel?: HotelType;
+  onSave: (hotelFormData: FormData) => void;
+  isLoading: boolean;
+};
 
-const ManageHotelForm = ({onSave,isLoading}:Props) => {
+const ManageHotelForm = ({ onSave, isLoading, hotel }: Props) => {
   const formMethods = useForm<HotelFormData>();
-  const {handleSubmit}=formMethods;
+  const { handleSubmit, reset } = formMethods;
 
-  const onSubmit=handleSubmit((formDataJson:HotelFormData)=>{
-    const formData=new FormData();
-    formData.append("name",formDataJson.name);
-    formData.append("city",formDataJson.city);
-    formData.append("country",formDataJson.country);
-    formData.append("description",formDataJson.description);
-    formData.append("type",formDataJson.type);
+  useEffect(() => {
+    reset(hotel);
+  }, [hotel, reset]);
+
+  const onSubmit = handleSubmit((formDataJson: HotelFormData) => {
+    const formData = new FormData();
+    if (hotel) {
+      formData.append("hotelId", hotel._id);
+    }
+    formData.append("name", formDataJson.name);
+    formData.append("city", formDataJson.city);
+    formData.append("country", formDataJson.country);
+    formData.append("description", formDataJson.description);
+    formData.append("type", formDataJson.type);
     formData.append("pricePerNight", formDataJson.pricePerNight.toString());
     formData.append("starRating", formDataJson.starRating.toString());
     formData.append("adultCount", formDataJson.adultCount.toString());
     formData.append("childCount", formDataJson.childCount.toString());
 
     formDataJson.facilities.forEach((facility, index) => {
-        formData.append(`facilities[${index}]`, facility);
-      });
-  
-    //   if (formDataJson.imageUrls) {
-    //     formDataJson.imageUrls.forEach((url, index) => {
-    //       formData.append(`imageUrls[${index}]`, url);
-    //     });
-    //   }
-  
-      Array.from(formDataJson.imageFiles).forEach((imageFile) => {
-        formData.append(`imageFiles`, imageFile);
-      });
+      formData.append(`facilities[${index}]`, facility);
+    });
 
-      onSave(formData)
-    
-    })
+    if (formDataJson.imageUrls) {
+      formDataJson.imageUrls.forEach((url, index) => {
+        formData.append(`imageUrls[${index}]`, url);
+      });
+    }
+
+    Array.from(formDataJson.imageFiles).forEach((imageFile) => {
+      formData.append(`imageFiles`, imageFile);
+    });
+
+    onSave(formData);
+  });
 
   return (
     <FormProvider {...formMethods}>
