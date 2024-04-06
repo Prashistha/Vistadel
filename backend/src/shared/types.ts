90b89c29be7876fa892ebf1a2cb72e49ef6b5fae
Nihar Phansalkar <phansalkarnihar@gmail.com>
2024-04-06 13:41:17 +0000

    added searching facility

--- backend/src/shared/types.ts
+++ backend/src/shared/types.ts
@@ -15,3 +15,39 @@ export type HotelType={
     lastUpdated:Date;
 
 }
+
+export type HotelSearchResponse = {
+  data: HotelType[];
+  pagination: {
+    total: number;
+    page: number;
+    pages: number;
+  };
+};
+
+export type BookingType = {
+  _id: string;
+  userId: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  adultCount: number;
+  childCount: number;
+  checkIn: Date;
+  checkOut: Date;
+  totalCost: number;
+};
+
+export type UserType = {
+  _id: string;
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+};
+
+export type PaymentIntentResponse = {
+  paymentIntentId: string;
+  clientSecret: string;
+  totalCost: number;
+};
