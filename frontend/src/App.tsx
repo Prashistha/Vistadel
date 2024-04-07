d5caaaa3d78294f4307f9286f0e88fb77e20c13c
OjaswiniKohale <ojaswini751@gmail.com>
2024-04-07 13:48:41 +0530

    changed the details page and created guestInfoForm

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,6 +13,7 @@ import { useAppContext } from "./contexts/AppContext";
 import MyHotels from "./pages/MyHotels";
 import EditHotel from "./pages/EditHotel";
 import Search from "./pages/Search";
+import Detail from "./pages/Details";
 
 const App = () => {
 	const { isLoggedIn } = useAppContext();
@@ -20,11 +21,12 @@ const App = () => {
 		<Router>
 			<Routes>
 				<Route path="/" element={<Layout>
-					<p>Home Page</p>
+				<p>Home Page</p>
 				</Layout>} />
 				<Route path="/search" element={<Layout><Search /></Layout>} />
 				<Route path="/register" element={<Layout><Register /></Layout>} />
 				<Route path="/sign-in" element={<Layout><SignIn /></Layout>} />
+				<Route path="/detail/:hotelId" element={<Layout><Detail /></Layout>} />
 
 				{isLoggedIn && <>
 					<Route path='/add-hotel'
