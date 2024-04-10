abe5038126e345b404d38295b373cc34fc63f7ec
Nihar Phansalkar <phansalkarnihar@gmail.com>
2024-04-10 13:16:58 +0000

    removed unused imports

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { useMutation, useQueryClient } from "react-query";
 import * as apiClient from "../api-client";
 import { useAppContext } from "../contexts/AppContext";
