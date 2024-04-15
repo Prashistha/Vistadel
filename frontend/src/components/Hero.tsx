d83e75a1fdd4b3df544cd98e44d9d28adf8d5d7b
Prashistha <Prashisthaco@gmail.com>
2024-04-15 09:46:22 +0530

    Made minor changes in UI

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -2,9 +2,9 @@ const Hero = () => {
     return (
       <div className="bg-[#BB6464] ">
         <div className="container mx-auto flex flex-col gap-2">
-          <h1 className="text-5xl text-white font-bold mb-4">Find your next stay</h1>
+          <h1 className="text-5xl text-white font-bold mb-4">Your perfect stay, just a click away.</h1>
         
-          <p className="text-2xl text-white">
+          <p className="text-2xl text-white font-bold">
             Search low prices on hotels for your dream vacation...
           </p>
         </div>
